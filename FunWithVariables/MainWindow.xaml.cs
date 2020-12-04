18401bc4bfd6d7a856d720980e442b01586ef5c1
BrentynMorgan35 <>
2020-12-04 09:23:18 -0600

    10/4/2020

---
+++ FunWithVariables/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FunWithVariables
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void butonMyMessage_Click(object sender, RoutedEventArgs e)
+        {
+
+            string firstName;
+
+            string lastName;
+
+            firstName = textBoxFirstName.Text;
+            lastName = textBoxLastName.Text;
+
+            labelMyMessage.Content = ("Hello " + firstName + " " + lastName + ", good to see you!");
+
+        }
+
+        private void buttonClearMe_Click(object sender, RoutedEventArgs e)
+        {
+
+            textBoxFirstName.Text = "";
+
+            textBoxLastName.Text = "";
+
+            labelMyMessage.Content = "";
+
+
+        }
+    }
+}
