52b7bbc57c113625d67cd625ba965f49f49ead0c
BrentynMorgan35 <54853239+BrentynMorgan35@users.noreply.github.com>
2020-12-14 10:04:18 -0600

    12/14/2020

---
+++ CS_ASP_010/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS_ASP_010
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+                        int i;
+
+                        i = "125"
+            */
+            /*
+            long j = 2000000000000;
+            
+            int i;
+            
+            i = (int) j;
+
+            labelAnswer.Content = i;
+            */
+
+            // double j = 2.55;
+            //int i = (int) j;
+            //labelAnswer.Content = i;
+
+
+            string userInput = textBoxInput.Text;
+
+            double numOne = double.Parse(userInput);
+
+            double anwswer = numOne + 64;
+
+            labelAnswer.Content = anwswer;
+
+        }
+    }
+}
